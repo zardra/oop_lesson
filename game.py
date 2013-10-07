2faa6f851422e7e3d8ce409f54c4ba25b795b3da
Hackbright Student <user@mini-007.(none)>
2013-10-07 14:58:54 -0700

    completed steps 12-13

--- game.py
+++ game.py
@@ -20,8 +20,13 @@ class Rock(GameElement):
     SOLID  = True
 
 class Character(GameElement):
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
     IMAGE = "Horns"
 
+    # defining how to move a character
     def next_pos(self, direction):
         if direction == "up":
             return (self.x, self.y-1)
@@ -32,10 +37,23 @@ class Character(GameElement):
         elif direction == "right":
             return (self.x+1, self.y)
         return None
+
+class Gem(GameElement):
+    
+    # how to add gems to a player's inventory
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" 
+            % (len(player.inventory)))
+
+    IMAGE = "BlueGem"
+    SOLID = False
+
 ####   End class definitions    ####
 def initialize():
     """Put game initialization code here"""
 
+    # initialize game with rocks in the following positions
     rock_positions = [
         (2, 1),
         (1, 2),
@@ -50,22 +68,27 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
+    # make the last rock in the list not solid 
     rocks[-1].SOLID = False
 
-    for rock in rocks:
-        print rock
-
+    # initialize game with player
     global PLAYER
     PLAYER = Character()
     GAME_BOARD.register(PLAYER)
     GAME_BOARD.set_el(2, 2, PLAYER)
-    print PLAYER
 
+    # message board
     GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+    # initialize game with gem
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
 
 def keyboard_handler():
     direction = None
 
+    # interprets keyboard commands
     if KEYBOARD[key.UP]:
         direction = "up"
     elif KEYBOARD[key.SPACE]:
@@ -76,13 +99,17 @@ def keyboard_handler():
         direction = "left"
     elif KEYBOARD[key.RIGHT]:
         direction = "right"
-
+    
+    # moves a character if allowed
     if direction:
         next_location = PLAYER.next_pos(direction)
         next_x = next_location[0]
         next_y = next_location[1]
 
         existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el:
+            existing_el.interact(PLAYER)
 
         if existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
