8eb3e9020f5be5b0867eaefa61f3fc0c32301614
Hackbright Student <user@mini-007.(none)>
2013-10-07 13:00:39 -0700

    Steps 9 - 11

--- game.py
+++ game.py
@@ -17,6 +17,7 @@ GAME_HEIGHT = 5
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
+    SOLID  = True
 
 class Character(GameElement):
     IMAGE = "Horns"
@@ -49,6 +50,8 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
+    rocks[-1].SOLID = False
+
     for rock in rocks:
         print rock
 
@@ -61,26 +64,28 @@ def initialize():
     GAME_BOARD.draw_msg("This game is wicked awesome.")
 
 def keyboard_handler():
+    direction = None
+
     if KEYBOARD[key.UP]:
-        GAME_BOARD.draw_msg("You pressed up")
-        next_y = PLAYER.y -1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+        direction = "up"
     elif KEYBOARD[key.SPACE]:
         GAME_BOARD.erase_msg()
     elif KEYBOARD[key.DOWN]:
-        GAME_BOARD.draw_msg("You pressed down")
-        next_y = PLAYER.y +1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+        direction = "down"
     elif KEYBOARD[key.LEFT]:
-        GAME_BOARD.draw_msg("You pressed left")
-        next_x = PLAYER.x -1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)    
+        direction = "left"
     elif KEYBOARD[key.RIGHT]:
-        GAME_BOARD.draw_msg("You pressed right")
-        next_x = PLAYER.x +1
-        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
+        direction = "right"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+        if existing_el is None or not existing_el.SOLID:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
 
