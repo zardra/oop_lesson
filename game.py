f36fbac242491571fe1f168a3185c2ca92ccff61
Hackbright Student <user@mini-007.(none)>
2013-10-07 12:45:00 -0700

    Steps 5 - 8

--- game.py
+++ game.py
@@ -17,33 +17,70 @@ GAME_HEIGHT = 5
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
-####   End class definitions    ####
 
+class Character(GameElement):
+    IMAGE = "Horns"
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+####   End class definitions    ####
 def initialize():
     """Put game initialization code here"""
 
-    #Initialize and register rock 1
-    rock1 = Rock()
-    GAME_BOARD.register(rock1)
-    GAME_BOARD.set_el(2, 1, rock1)
-    
-    # Initialize and register rock 2
-    rock2 = Rock()
-    GAME_BOARD.register(rock2)
-    GAME_BOARD.set_el(1, 2, rock2)
+    rock_positions = [
+        (2, 1),
+        (1, 2),
+        (3, 2),
+        (2, 3),
+    ]
 
-    # Initialize and register rock 3
-    rock3 = Rock()
-    GAME_BOARD.register(rock3)
-    GAME_BOARD.set_el(3, 2, rock3)
+    rocks = []
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
 
-    # Initialize and register rock 4
-    rock4 = Rock()
-    GAME_BOARD.register(rock4)
-    GAME_BOARD.set_el(2, 3, rock4)
+    for rock in rocks:
+        print rock
 
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2, 2, PLAYER)
+    print PLAYER
 
-    print "The first rock is at", (rock1.x, rock1.y)
-    print "The second rock is at", (rock2.x, rock2.y)
-    print "Rock 1 image", rock1.IMAGE
-    print "Rock 2 image", rock2.IMAGE
+    GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+def keyboard_handler():
+    if KEYBOARD[key.UP]:
+        GAME_BOARD.draw_msg("You pressed up")
+        next_y = PLAYER.y -1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    elif KEYBOARD[key.SPACE]:
+        GAME_BOARD.erase_msg()
+    elif KEYBOARD[key.DOWN]:
+        GAME_BOARD.draw_msg("You pressed down")
+        next_y = PLAYER.y +1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(PLAYER.x, next_y, PLAYER)
+    elif KEYBOARD[key.LEFT]:
+        GAME_BOARD.draw_msg("You pressed left")
+        next_x = PLAYER.x -1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)    
+    elif KEYBOARD[key.RIGHT]:
+        GAME_BOARD.draw_msg("You pressed right")
+        next_x = PLAYER.x +1
+        GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+        GAME_BOARD.set_el(next_x, PLAYER.y, PLAYER)
+
