99114a881d202673c4ff7a9c355b2521d37a305e
Hackbright Student <user@mini-007.(none)>
2013-10-07 17:30:22 -0700

    Step 14

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pyglet
 from pyglet.window import key
 from core import GameElement
 import sys
+from random import randint
 
 #### DO NOT TOUCH ####
 GAME_BOARD = None
@@ -11,8 +12,8 @@ KEYBOARD = None
 PLAYER = None
 ######################
 
-GAME_WIDTH = 5
-GAME_HEIGHT = 5
+GAME_WIDTH = 8
+GAME_HEIGHT = 8
 
 #### Put class definitions here ####
 class Rock(GameElement):
@@ -49,6 +50,32 @@ class Gem(GameElement):
     IMAGE = "BlueGem"
     SOLID = False
 
+class Key(GameElement):
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a key!")
+        print player.inventory
+
+    IMAGE = "Key"
+    SOLID = False
+
+class Chest(GameElement):
+
+    def interact(self, player):
+        for i in player.inventory:
+            if type(i) == Key:
+                chest2 = Chest()
+                chest2.IMAGE = "ChestOpen"
+                GAME_BOARD.register(chest2)
+                GAME_BOARD.del_el(self.x, self.y)
+                GAME_BOARD.set_el(self.x, self.y, chest2)
+                chest2.SOLID = False
+
+    IMAGE = "Chest"
+    SOLID = True
+
+
 ####   End class definitions    ####
 def initialize():
     """Put game initialization code here"""
@@ -71,19 +98,43 @@ def initialize():
     # make the last rock in the list not solid 
     rocks[-1].SOLID = False
 
-    # initialize game with player
+ 
+
+    # message board
+    GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+    # initialize game with objects
+    gem1 = Gem()
+    GAME_BOARD.register(gem1)
+    GAME_BOARD.set_el(3, 1, gem1)
+
+    gem2 = Gem()
+    gem2.IMAGE = "OrangeGem"
+    GAME_BOARD.register(gem2)
+    GAME_BOARD.set_el(3, 3, gem2)
+
+
+    chest = Chest()
+    GAME_BOARD.register(chest)
+    GAME_BOARD.set_el(5, 5, chest)
+
+   # initialize game with player
     global PLAYER
     PLAYER = Character()
     GAME_BOARD.register(PLAYER)
     GAME_BOARD.set_el(2, 2, PLAYER)
 
-    # message board
-    GAME_BOARD.draw_msg("This game is wicked awesome.")
+    a_key = Key()
+    GAME_BOARD.register(a_key)
 
-    # initialize game with gem
-    gem = Gem()
-    GAME_BOARD.register(gem)
-    GAME_BOARD.set_el(3, 1, gem)
+    rand_x = randint(0, GAME_WIDTH-1)
+    rand_y = randint(0, GAME_HEIGHT-1)
+
+    while rand_x == PLAYER.x or rand_y == PLAYER.y:
+        rand_x = randint(0, GAME_WIDTH-1)
+        rand_y = randint(0, GAME_HEIGHT-1)
+    else:
+        GAME_BOARD.set_el(rand_x, rand_y, a_key)
 
 def keyboard_handler():
     direction = None
@@ -106,13 +157,25 @@ def keyboard_handler():
         next_x = next_location[0]
         next_y = next_location[1]
 
-        existing_el = GAME_BOARD.get_el(next_x, next_y)
+        GAME_BOARD.erase_msg()
 
-        if existing_el:
-            existing_el.interact(PLAYER)
+        # looks at next spot on the board
+        if 0 <= next_x < GAME_WIDTH and 0 <= next_y < GAME_WIDTH:
 
-        if existing_el is None or not existing_el.SOLID:
-            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-            GAME_BOARD.set_el(next_x, next_y, PLAYER)
+            existing_el = GAME_BOARD.get_el(next_x, next_y)
+
+            # interact with player
+            if existing_el:
+                existing_el.interact(PLAYER)
+
+            # check if the next spot can be moved to
+            if existing_el is None or not existing_el.SOLID:
+                GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+                GAME_BOARD.set_el(next_x, next_y, PLAYER)
+
+        else:
+            GAME_BOARD.draw_msg("Don't go off the board!")
+
+
 
 
